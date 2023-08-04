a5235244d5619d7b84b3f31489e7db469a8ce25c
Huberto Solorzano <hsolorzano796@gmail.com>
2023-08-04 16:56:24 +0000

    CARGA INICIAL

---
+++ fiorutest/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/mx/gs/fiorutest/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ fiorutest/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"commxgs/fiorutest/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
